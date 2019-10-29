b3aacc04ab71d7d6e4da5219706d19f591eabe94
Alex Dortzbach <alexdortzbach@Alexs-MacBook-Pro-3.local>
2019-10-29 16:25:21 -0400

    view directing

--- src/apollo/settings.py
+++ src/apollo/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'apollo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'login/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
